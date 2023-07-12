66ebb45eddf5081602c4b2fe997a9b83d014440c
triplee12 <tripleeoliver@gmail.com>
2023-07-12 22:02:47 +0100

    Add bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,26 @@
+-- Add bonus
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score FLOAT
+)
+BEGIN
+    DECLARE project_id INT;
+    
+    -- Check if the project already exists
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    
+    -- If the project doesn't exist, create it
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    
+    -- Add the correction for the student
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+    
+END //
+
+DELIMITER ;
