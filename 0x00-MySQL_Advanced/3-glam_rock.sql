a982a2af82bb7fd3cd9c5b6db19ba8412a618232
triplee12 <tripleeoliver@gmail.com>
2023-07-13 11:12:47 +0100

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,11 @@
 -- Old school band
--- This SQL script select band_name, and lifespan column which is difference
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT band_name, 
+    (YEAR('2022-01-01') - YEAR(Formed)) - 
+        CASE 
+            WHEN MONTH('2022-01-01') < MONTH(Formed) THEN 1
+            WHEN MONTH('2022-01-01') = MONTH(Formed) AND DAY('2022-01-01') < DAY(Formed) THEN 1
+            ELSE 0
+        END AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
     ORDER BY lifespan DESC;
