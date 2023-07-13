2b6d9a5aba4c677fcca5b8e6c7fd050e2e1bf96a
triplee12 <tripleeoliver@gmail.com>
2023-07-13 11:04:27 +0100

    Fix count error, change count to sum

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Best band ever!
-SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
