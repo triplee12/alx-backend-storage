8a17d86d33fb5c95eec50a6677e6f75eb274c9f1
triplee12 <tripleeoliver@gmail.com>
2023-07-19 11:42:18 +0100

    Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Insert a document in Python."""
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a documents into a collection."""
+    data = mongo_collection.insert_one(kwargs)
+    return data.inserted_id
