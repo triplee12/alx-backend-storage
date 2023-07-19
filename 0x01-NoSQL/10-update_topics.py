e4d53ac92ecde198be0a17f3d16ac37be11c6811
triplee12 <tripleeoliver@gmail.com>
2023-07-19 11:48:39 +0100

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Change school topics."""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update document with a specific attr: value."""
+    return mongo_collection.update_many({
+            "name": name
+        },
+        {
+            "$set": {
+                "topics": topics
+            }
+        })
